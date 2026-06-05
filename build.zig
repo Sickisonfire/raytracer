324c407f4765b1b6a8b7f19b58e7afb6d9403dec
Marcel <sickisonfire@duck.com>
2026-06-05 21:58:39 +0200

    feat: add build command "clean" to delete ppm

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Io = std.Io;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -23,6 +24,18 @@ pub fn build(b: *std.Build) void {
     show_cmd.addArgs(&.{ "+kitten", "icat", "./zig-out/out.ppm" });
     show_cmd.step.dependOn(&run_cmd.step);
 
+    const clean_step = b.step("clean", "Clean");
+    clean_step.makeFn = cleanStepFn;
+
     const debug_step = b.step("show", "Show the resulting ppm in the terminal");
     debug_step.dependOn(&show_cmd.step);
+}
+
+fn cleanStepFn(step: *std.Build.Step, _: std.Build.Step.MakeOptions) !void {
+    const io = step.owner.graph.io;
+    const dir = std.Io.Dir.cwd().openDir(io, "zig-out", .{}) catch unreachable;
+    defer dir.close(io);
+    dir.deleteFile(io, "out.ppm") catch {
+        std.debug.print("nothing to clean\n", .{});
+    };
 }
