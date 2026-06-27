f4f39e969dd71667b06261e161a4b7e174aac388
Marcel <sickisonfire@duck.com>
2026-06-27 21:47:49 +0200

    feat: colored sphere

--- src/ray.zig
+++ src/ray.zig
@@ -23,38 +23,36 @@ pub fn getColor(self: *Ray) Color {
     // lerp
     // start_color + (end_color - start_color)a
     const a = 0.5 * (unit_dir.y() + 1.0);
-    var start_color: Color = .new(1.0, 1.0, 1.0);
-    var end_color: Color = .new(0.5, 0.7, 1.0);
+    const t = hitSphere(.new(0, 0, -1), 0.5, self);
 
-    const ret = start_color.add(&end_color.sub(&start_color).multScalar(a));
-    // end_color.sub(start_color);
-    // end_color.multScalar(a);
-    // start_color.add(end_color);
-
-    // const x = unit_dir.x();
-    // std.debug.print("{any}", .{unit_dir});
-
-    if (hitSphere(.new(0, 0, -1), 0.5, self)) {
-        return .new(1, 0, 0);
+    if (t > 0) {
+        const n: Vec3 = Vec3.unitVector(&self.at(t).sub(&.new(0, 0, -1)));
+        return Vec3.new(n.x() + 1, n.y() + 1, n.z() + 1).multScalar(0.5);
     }
+    const start_color: Color = .new(1.0, 1.0, 1.0);
+    const end_color: Color = .new(0.5, 0.7, 1.0);
+    const ret = start_color.add(&end_color.sub(&start_color).multScalar(a));
 
     return ret;
 }
 
-fn hitSphere(center: Point3, radius: f64, ray: *Ray) bool {
-    var oc = &center.sub(&ray.origin);
-    // oc.sub(ray.origin);
-    const a = ray.direction.dot(&ray.direction);
-    const b = ray.direction.dot(oc) * -2;
-    const c = oc.dot(oc) - radius * radius;
-    const discriminant = b * b - 4 * a * c;
+fn hitSphere(center: Point3, radius: f64, ray: *Ray) f64 {
+    const oc: Vec3 = center.sub(&ray.origin);
+    const a: f64 = ray.direction.lengthSquared();
+    const h: f64 = ray.direction.dot(&oc);
+    const c: f64 = oc.lengthSquared() - radius * radius;
+    const discriminant: f64 = h * h - a * c;
 
-    return (discriminant >= 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - std.math.sqrt(discriminant)) / a;
+    }
 }
 
 test "at" {
     const direction = Vec3{ .inner = .{ 1, 2, 3 } };
-    var ray: Ray = .new(.zero, direction);
+    var ray: Ray = .new(.zero, &direction);
 
     const actual = ray.at(2);
 
